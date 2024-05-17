496f5788cd678bfe09636ed25a1b4fe0afaaaae1
Evgeniy_Zhelak <evgeniyzhelak@gmail.com>
2024-05-17 19:42:22 +0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -113,3 +113,26 @@ class TicTacToe:
         print(bottom_line)
         print(numeration_h)
         print("-" * 25)
+
+
+
+game = TicTacToe()
+game.init()
+
+step_game = 0
+
+while game:
+    game.show()
+    if step_game % 2 == 0:
+        game.human_go()
+    else:
+        game.computer_go()
+    step_game += 1
+game.show()
+
+if game.is_human_win:
+    print("Поздравляем! Вы победили!")
+elif game.is_computer_win:
+    print("Все получится, со временем")
+else:
+    print("Ничья.")
